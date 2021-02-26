dc1effb09a4d5df4c3a8c07b09fbd28667185d0c
LeeJack430 <79582865+LeeJack430@users.noreply.github.com>
2021-02-26 20:59:32 +0800

    Add files via upload

---
+++ 0224作業01.py
@@ -0,0 +1,16 @@
+number=int(input("請輸入次數:"))
+lisT1=[]
+lisT2=[]
+
+for i in range(1,number+1):
+     if i %2 == 0 :
+         lisT1.append(i)
+
+for I in range(1,number+1):
+    if I %2 == 1 :
+        lisT2.append(I)
+        
+
+print(lisT1,"皆為偶數")
+print(lisT2,"皆為奇數")
+print("程式結束")
